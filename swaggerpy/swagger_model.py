6eedef6baf6beee0967dfca362152772206ea3f2
David M. Lee <dlee@digium.com>
2013-08-17 00:46:46 -0500

    organized

--- swaggerpy/swagger_model.py
+++ swaggerpy/swagger_model.py
@@ -51,46 +51,45 @@ class ParsingContext(object):
     stack), use the next() and next_stack() functions to build a new one.
     """
 
-    def __init__(self, swagger_version, stack):
-        self.__swagger_version = swagger_version
-        self.__stack = stack
+    def __init__(self):
+        self.type_stack = []
+        self.id_stack = []
+        self.args = {}
 
     def __repr__(self):
-        return "ParsingContext(swagger_version=%s, stack=%s)" % (
-            self.swagger_version, self.stack)
+        return "ParsingContext(stack=%s)" % self.type_stack
 
-    def get_swagger_version(self):
-        return self.__swagger_version
+    def push(self, obj_type, json, id_field):
+        """Pushes a new self-identifying object into the context.
 
-    def get_stack(self):
-        return self.__stack
-
-    swagger_version = property(get_swagger_version)
-
-    stack = property(get_stack)
-
-    def version_less_than(self, ver):
-        return compare_versions(self.swagger_version, ver) < 0
-
-    def next_stack(self, json, id_field):
-        """Returns a new item pushed to the stack.
-
-        @param json: Current JSON object.
-        @param id_field: Field identifying this object.
-        @return New context with additional item in the stack.
+        @type obj_type: str
+        @param obj_type: Specifies type of object json represents
+        @type json: Jsonified
+        @param json: Current Jsonified object.
+        @type id_field: str
+        @param id_field: Field name in json that identifies it.
         """
-        if not id_field in json:
+        if id_field not in json:
             raise SwaggerError("Missing id_field: %s" % id_field, self)
-        new_stack = self.stack + ['%s=%s' % (id_field, str(json[id_field]))]
-        return ParsingContext(self.swagger_version, new_stack)
+        self.push_str(obj_type, json, '%s=%s' % (obj_type, str(json[id_field])))
 
-    def next(self, version=None, stack=None):
-        if version is None:
-            version = self.swagger_version
-        if stack is None:
-            stack = self.stack
-        return ParsingContext(version, stack)
+    def push_str(self, obj_type, json, id_string):
+        """Pushes a new  object into the context.
 
+        @type obj_type: str
+        @param obj_type: Specifies type of object json represents
+        @type json: Jsonified
+        @param json: Current Jsonified object.
+        @type id_string: str
+        @param id_string: Identifier of the given json.
+        """
+        self.type_stack.append(obj_type)
+        self.id_stack.append(id_string)
+        self.args[obj_type] = json
+
+    def pop(self):
+        self.type_stack.pop()
+        del self.args[self.type_stack.pop()]
 
 class SwaggerError(Exception):
     """Raised when an error is encountered mapping the JSON objects into the
@@ -107,85 +106,134 @@ class SwaggerError(Exception):
         super(Exception, self).__init__(msg, context, cause)
 
 
-class SwaggerPostProcessor(object):
-    """Post processing interface for model objects.
+class SwaggerProcessor(object):
+    """Post processing interface for Swagger API's.
 
     This processor can add fields to model objects for additional
     information to use in the templates.
-
     """
+
+    def apply(self, resources):
+        context = ParsingContext()
+        context.push_str('resources', resources, resources.file)
+        self.process_resource_listing(**context.args)
+        for api in resources.apis:
+            context.push('api', api, api.path)
+            self.process_resource_listing_api(**context.args)
+            context.pop()
+
+            context.push_str('api_declaration', api.api_declaration, api.file)
+            self.process_api_declaration(**context.args)
+            for resource_api in api.api_declaration.apis:
+                context.push('resource_api', resource_api, 'path')
+                self.process_resource_api(**context.args)
+                for operation in resource_api.operations:
+                    context.push('operation', operation, 'nickname')
+                    self.process_operation(**context.args)
+                    for parameter in operation.parameters:
+                        context.push('parameter', parameter, 'name')
+                        self.process_parameter(**context.args)
+                        context.pop()
+                    for response in operation.error_responses:
+                        context.push('error_response', response, 'code')
+                        self.process_error_response(**context.args)
+                        context.pop()
+                    context.pop()
+                context.pop()
+            for (model_name, model) in api.api_declaration.models:
+                context.push('model', model, 'id')
+                self.process_model(**context.args)
+                if model_name != model.id:
+                    raise SwaggerError("Model id doesn't match name", context)
+                for (prop_name, prop) in model.properties:
+                    context.push_str('property', prop, prop_name)
+                    prop.name = prop_name
+                    self.process_property(**context.args)
+                    context.pop()
+            context.pop()
 
     def process_resource_listing(self, resources, context):
         """Post process a resources.json object.
 
         @param resources: ResourceApi object.
+        @type context: ParsingContext
         @param context: Current context in the API.
         """
         pass
 
-    def process_resource_listing_api(self, resource_api, context):
+    def process_resource_listing_api(self, resources, resource_api, context):
         """Post process entries in a resource.json's api array.
 
         @param resource_api: ResourceApi object.
+        @type context: ParsingContext
         @param context: Current context in the API.
         """
         pass
 
-    def process_api_declaration(self, resource, context):
+    def process_api_declaration(self, resources, api_declaration, context):
         """Post process a resource object.
 
         This is parsed from a .json file reference by a resource listing's 'api'
         array.
 
-        @param resource: resource object.
+        @param api_declaration: resource object.
+        @type context: ParsingContext
         @param context: Current context in the API.
         """
         pass
 
-    def process_resource_api(self, api, context):
+    def process_resource_api(self, resources, api_declaration, api, context):
         """Post process entries in a resource's api array
 
         @param api: API object
+        @type context: ParsingContext
         @param context: Current context in the API.
         """
         pass
 
-    def process_operation(self, operation, context):
+    def process_operation(self, resources, resource, api, operation, context):
         """Post process an operation on an api.
 
         @param operation: Operation object.
+        @type context: ParsingContext
         @param context: Current context in the API.
         """
         pass
 
-    def process_parameter(self, parameter, context):
+    def process_parameter(self, resources, resource, api, operation, parameter,
+                          context):
         """Post process a parameter on an operation.
 
         @param parameter: Parameter object.
+        @type context: ParsingContext
         @param context: Current context in the API.
         """
         pass
 
-    def process_error_response(self, response, context):
+    def process_error_response(self, resources, resource, api, operation,
+                               error_response, context):
         """Post process an errorResponse on an operation.
 
-        @param response: Parameter object.
+        @param error_response: Response object.
+        @type context: ParsingContext
         @param context: Current context in the API.
         """
         pass
 
-    def process_model(self, model, context):
+    def process_model(self, resources, resource, model, context):
         """Post process a model from a resources model dictionary.
 
         @param model: Model object.
+        @type context: ParsingContext
         @param context: Current context in the API.
         """
         pass
 
-    def process_property(self, prop, context):
+    def process_property(self, resources, resource, model, prop, context):
         """Post process a property from a model.
 
         @param prop: Property object.
+        @type context: ParsingContext
         @param context: Current context in the API.
         """
         pass
@@ -194,198 +242,97 @@ class SwaggerPostProcessor(object):
         """Post process a type.
 
         @param swagger_type: ResourceListing object.
+        @type context: ParsingContext
         @param context: Current context in the API.
         """
         pass
 
 
-def load_resource_listing(resources_file, processor):
-    """Load a resource listing.
+class PrimaryProcessor(SwaggerProcessor):
+    def process_resource_listing(self, resources, context):
+        required_fields = ['apiVersion', 'basePath', 'apis', 'swaggerVersion']
+        validate_required_fields(resources, required_fields, context)
 
-    @param resources_file: File name for resources.json
-    @type processor: SwaggerPostProcessor
-    @param processor: Custom SwaggerPostProcessor.
-    """
-    context = ParsingContext(None, [resources_file])
-    with open(resources_file) as fp:
-        resources = Jsonified(json.load(fp))
+        if not resources.swaggerVersion in SWAGGER_VERSIONS:
+            raise SwaggerError(
+                "Unsupported Swagger version %s" % resources.swaggerVersion,
+                context)
+
+        for api in resources.apis:
+            self.process_resource_listing_api(resources, api, context)
+
+    def process_resource_listing_api(self, resources, api, context):
+        context = context.next_stack(api, 'path')
+        validate_required_fields(api, ['path', 'description'], context)
+
+        if not api.path.startswith("/"):
+            raise SwaggerError("Path must start with /", context)
+
+    def process_api_declaration(self, resources, api_declaration, context):
+        required_fields = [
+            'swaggerVersion', 'apiVersion', 'basePath', 'resourcePath', 'apis',
+            'models'
+        ]
+        validate_required_fields(api_declaration, required_fields, context)
+
+    def process_resource_api(self, resources, api_declaration, api, context):
+        pass
+
+    def process_operation(self, resources, resource, api, operation, context):
+        pass
+
+    def process_parameter(self, resources, resource, api, operation, parameter,
+                          context):
+        pass
+
+    def process_error_response(self, resources, resource, api, operation,
+                               response, context):
+        pass
+
+    def process_model(self, resources, resource, model, context):
+        pass
+
+    def process_property(self, resources, resource, model, prop, context):
+        pass
+
+    def process_type(self, swagger_type, context):
+        pass
+
+
+class Loader(object):
+    def __init__(self, processors=None):
+        if processors is None:
+            processors = []
+            # Always go through primary processing first
+        processors.insert(0, PrimaryProcessor())
+        self.processors = processors
+
+    def load_resource_listing(self, resources_file):
+        """Load a resource listing.
+
+        @param resources_file: File name for resources.json
+        """
+
+        # Load the resource listing
+        with open(resources_file) as fp:
+            resources = Jsonified(json.load(fp))
+
+        # Some extra data only known about at load time
         resources.file = resources_file
         resources.base_dir = os.path.dirname(resources_file)
-        return process_resource_listing(resources, processor,
-                                        context)
 
+        # Load the API declarations
+        for api in resources.apis:
+            self.load_api_declaration(resources, api)
 
-def process_resource_listing(resources, processor, context):
-    """Post process entries in a resource's api array
+        # Now that the raw object model has been loaded, apply the processors
+        for processor in self.processors:
+            processor.apply(resources)
 
-    @param resources: Resources object.
-    @type processor: SwaggerPostProcessor
-    @param processor: Custom SwaggerPostProcessor.
-    @type context: ParsingContext
-    @param context: Current context in the API.
-
-    """
-    required_fields = ['apiVersion', 'basePath', 'apis', 'swaggerVersion']
-    validate_required_fields(resources, required_fields, context)
-
-    if not resources.swaggerVersion in SWAGGER_VERSIONS:
-        raise SwaggerError(
-            "Unsupported Swagger version %s" % resources.swaggerVersion,
-            context)
-
-    for api in resources.apis:
-        process_resource_listing_api(api, processor, resources.base_dir,
-                                     context)
-    processor.process_resource_listing(resources, context)
-    return resources
-
-
-def process_resource_listing_api(api, processor, base_dir, context):
-    """Post process entries in a resource's api array
-
-    @param api: API object.
-    @type processor: SwaggerPostProcessor
-    @param processor: Custom SwaggerPostProcessor.
-    @type context: ParsingContext
-    @param context: Current context in the API.
-
-    """
-    context = context.next_stack(api, 'path')
-    validate_required_fields(api, ['path', 'description'], context)
-
-    if not api.path.startswith("/"):
-        raise SwaggerError("Path must start with /", context)
-
-    api.file = (base_dir + api.path).replace('{format}', 'json')
-
-    api.api_declaration = load_api_declaration(api.file, processor)
-
-    processor.process_resource_api(api, context)
-    return api
-
-
-def load_api_declaration(api_file, processor):
-    context = ParsingContext(None, [api_file])
-    with open(api_file) as fp:
-        api_declaration = Jsonified(json.load(fp))
-        api_declaration.file = api_file
-        process_api_declaration(api_declaration, processor, context)
-        return api_declaration
-
-
-def process_api_declaration(api_declaration, processor, context):
-    """Post process entries in a resource's api array
-
-    @param api_declaration: API declaration object.
-    @type processor: SwaggerPostProcessor
-    @param processor: Custom SwaggerPostProcessor.
-    @type context: ParsingContext
-    @param context: Current context in the API.
-    """
-    required_fields = [
-        'swaggerVersion', 'apiVersion', 'basePath', 'resourcePath', 'apis',
-        'models'
-    ]
-    validate_required_fields(api_declaration, required_fields, context)
-
-    for api in api_declaration.apis:
-        process_resource_api(api, processor, context)
-
-    # Extract models from object to an array, for better mustache-ness
-    api_declaration.models = [
-        process_model(model, model_id, processor, context)
-        for (model_id, model) in api_declaration.models
-    ]
-
-    processor.process_api_declaration(api_declaration, context)
-    return api_declaration
-
-
-def process_resource_api(api, processor, context):
-    """Post process entries in a resource's api array
-
-    @param api: API object.
-    @type processor: SwaggerPostProcessor
-    @param processor: Custom SwaggerPostProcessor.
-    @type context: ParsingContext
-    @param context: Current context in the API.
-
-    """
-    pass
-
-
-def process_operation(operation, processor, context):
-    """Post process an operation on an api.
-
-    @param operation: Operation object.
-    @type processor: SwaggerPostProcessor
-    @param processor: Custom SwaggerPostProcessor.
-    @type context: ParsingContext
-    @param context: Current context in the API.
-    """
-    pass
-
-
-def process_parameter(parameter, processor, context):
-    """Post process a parameter on an operation.
-
-    @param parameter: Parameter object.
-    @type processor: SwaggerPostProcessor
-    @param processor: Custom SwaggerPostProcessor.
-    @type context: ParsingContext
-    @param context: Current context in the API.
-    """
-    pass
-
-
-def process_error_response(response, processor, context):
-    """Post process an errorResponse on an operation.
-
-    @param response: Parameter object.
-    @type processor: SwaggerPostProcessor
-    @param processor: Custom SwaggerPostProcessor.
-    @type context: ParsingContext
-    @param context: Current context in the API.
-    """
-    pass
-
-
-def process_model(model, model_id, processor, context):
-    """Post process a model from a resources model dictionary.
-
-    @param model: Model object.
-    @type processor: SwaggerPostProcessor
-    @param processor: Custom SwaggerPostProcessor.
-    @type context: ParsingContext
-    @param context: Current context in the API.
-    """
-    pass
-
-
-def process_property(prop, processor, context):
-    """Post process a property from a model.
-
-    @param prop: Property object.
-    @type processor: SwaggerPostProcessor
-    @param processor: Custom SwaggerPostProcessor.
-    @type context: ParsingContext
-    @param context: Current context in the API.
-    """
-    pass
-
-
-def process_type(swagger_type, processor, context):
-    """Post process a type.
-
-    @param swagger_type: ResourceListing object.
-    @type processor: SwaggerPostProcessor
-    @param processor: Custom SwaggerPostProcessor.
-    @type context: ParsingContext
-    @param context: Current context in the API.
-    """
-    pass
-
-
+    def load_api_declaration(self, resources, api):
+        api.file = (resources.base_dir + api.path).replace('{format}', 'json')
+        with open(api.file) as fp:
+            api.api_declaration = Jsonified(json.load(fp))
 def validate_required_fields(json, required_fields, context):
     """Checks a JSON object for a set of required fields.
 
