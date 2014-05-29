27371a9af1f621a8697e706b59dbe6c6fa48af71
Prateek Agarwal <prateek@yelp.com>
2014-05-29 16:10:16 -0700

    Add more tests for http swagger

---
+++ swaggerpy_test/client_http_test.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python
+
+#
+# Copyright (c) 2013, Digium, Inc.
+#
+
+"""Swagger client tests.
+"""
+
+import httpretty
+import unittest
+
+from swaggerpy.client import SwaggerClient
+
+
+# noinspection PyDocstring
+class ClientTest(unittest.TestCase):
+
+    @httpretty.activate
+    def test_bad_operation(self):
+        try:
+            self.uut.simple.doesNotExist()
+            self.fail("Expected attribute error")
+        except AttributeError:
+            pass
+
+    @httpretty.activate
+    def test_bad_param(self):
+        try:
+            self.uut.simple.getAsteriskInfo(doesNotExist='asdf')
+            self.fail("Expected type error")
+        except TypeError:
+            pass
+
+    @httpretty.activate
+    def test_missing_required(self):
+        try:
+            self.uut.simple1.getAsteriskInfoHttp()
+            self.fail("Expected type error")
+        except TypeError:
+            pass
+
+    #Use baesPath mentioned in the API declaration if it is not '/'
+    @httpretty.activate
+    def test_get(self):
+        httpretty.register_uri(
+            httpretty.GET, "http://localhost/swagger/test/test?test_param=foo",
+            body='[]')
+
+        resp = self.uut.simple.getAsteriskInfo(test_param="foo")
+        self.assertEqual(200, resp.status_code)
+        self.assertEqual([], resp.json())
+
+    #Use baesPath as api domain if it is '/' in the API declaration
+    @httpretty.activate
+    def test_get_check_basePath(self):
+        httpretty.register_uri(
+            httpretty.GET, "http://localhost/test_http?test_param=foo",
+            body='[]')
+
+        resp = self.uut.simple1.getAsteriskInfoHttp(test_param="foo")
+        self.assertEqual(200, resp.status_code)
+        self.assertEqual([], resp.json())
+
+    @httpretty.activate
+    def setUp(self):
+        httpretty.register_uri(
+            httpretty.GET, "http://localhost/api-docs",
+            body=open('test-data/1.2/simple/resources.json','r').read())
+
+        httpretty.register_uri(
+            httpretty.GET, "http://localhost/api-docs/simple.json",
+            body=open('test-data/1.2/simple/simple.json','r').read())
+
+        httpretty.register_uri(
+            httpretty.GET, "http://localhost/api-docs/simple1.json",
+            body=open('test-data/1.2/simple/simple1.json','r').read())
+
+        # Default handlers for all swagger.py access
+        self.uut = SwaggerClient(u'http://localhost/api-docs')
+
+
+if __name__ == '__main__':
+    unittest.main()
