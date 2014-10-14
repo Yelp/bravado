8ff6c0c1988fcb1ac520b4fb1350a3495efd395c
William Ting <wting@yelp.com>
2014-10-13 18:41:29 -0700

    Update setup.py.

--- setup.py
+++ setup.py
@@ -1,12 +1,6 @@
 #!/usr/bin/env python
-
-#
 # Copyright (c) 2013, Digium, Inc.
 # Copyright (c) 2014, Yelp, Inc.
-#
-
-"""Setup script
-"""
 
 import os
 
@@ -22,12 +16,12 @@ setup(
     description="Library for accessing Swagger-enabled API's",
     long_description=open(os.path.join(os.path.dirname(__file__),
                                        "README.rst")).read(),
-    author="Digium, Inc.",
-    author_email="dlee@digium.com",
+    author="Yelp, Inc.",
+    author_email="opensource+swaggerpy@yelp.com",
     url="https://github.com/Yelp/swagger-py",
     packages=["swaggerpy"],
     classifiers=[
-        "Development Status :: 1 - Planning",
+        "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
         "Topic :: Software Development :: Libraries :: Python Modules",
         "License :: OSI Approved :: BSD License",
