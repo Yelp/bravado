88e5f837de4bcebcf160fc63fa19e99ef8122e43
Bradley Barry <bkbarry@140-sfoeng49-231.clients.corp.yelpcorp.com>
2015-03-24 10:51:57 -0700

    Change unicode->string cast to use encode()

--- swaggerpy/client.py
+++ swaggerpy/client.py
@@ -180,7 +180,7 @@ class Operation(object):
         request = {}
         # The requests library expects native strings. Without this, POST
         # with a binary file upload throws a UnicodeDecodeError.
-        request['method'] = str(self._json[u'method'])
+        request['method'] = self._json[u'method'].encode('utf8')
         request['url'] = self._uri
         request['params'] = {}
         request['headers'] = _request_options.get('headers', {}) or {}
