ade153b7d70b58a521f07d736dfb846120a13aeb
Bryan Marty <bmarty@yelp.com>
2016-09-09 12:33:33 -0700

    More exceptions

--- bravado/exception.py
+++ bravado/exception.py
@@ -53,42 +53,6 @@ class HTTPError(with_metaclass(HTTPErrorType, IOError)):
         return '{0}{1}{2}'.format(status_and_reason, message, result)
 
 
-class HTTPClientError(HTTPError):
-    """4xx responses."""
-
-
-class HTTPBadRequest(HTTPClientError):
-    status_code = 400
-
-
-class HTTPUnauthorized(HTTPClientError):
-    status_code = 401
-
-
-class HTTPForbidden(HTTPClientError):
-    status_code = 403
-
-
-class HTTPNotFound(HTTPClientError):
-    status_code = 404
-
-
-class HTTPServerError(HTTPError):
-    """5xx responses."""
-
-
-class HTTPInternalServerError(HTTPServerError):
-    status_code = 500
-
-
-class HTTPBadGateway(HTTPServerError):
-    status_code = 503
-
-
-class HTTPGatewayTimeout(HTTPServerError):
-    status_code = 504
-
-
 def make_http_exception(response, message=None, swagger_result=None):
     """
     Return an HTTP exception class  based on the response. If a specific
@@ -103,3 +67,166 @@ def make_http_exception(response, message=None, swagger_result=None):
     """
     exc_class = status_map.get(response.status_code, HTTPError)
     return exc_class(response, message=message, swagger_result=swagger_result)
+
+
+class HTTPClientError(HTTPError):
+    """4xx responses."""
+
+
+class HTTPServerError(HTTPError):
+    """5xx responses."""
+
+
+# The follow are based on the HTTP Status Code Registery at
+# http://www.iana.org/assignments/http-status-codes/http-status-codes.xhtml
+
+class HTTPBadRequest(HTTPClientError):
+    status_code = 400
+
+
+class HTTPUnauthorized(HTTPClientError):
+    status_code = 401
+
+
+class HTTPPaymentRequired(HTTPClientError):
+    status_code = 402
+
+
+class HTTPForbidden(HTTPClientError):
+    status_code = 403
+
+
+class HTTPNotFound(HTTPClientError):
+    status_code = 404
+
+
+class HTTPMethodNotAllowed(HTTPClientError):
+    status_code = 405
+
+
+class HTTPNotAcceptable(HTTPClientError):
+    status_code = 406
+
+
+class HTTPProxyAuthenticationRequired(HTTPClientError):
+    status_code = 407
+
+
+class HTTPRequestTimeout(HTTPClientError):
+    status_code = 408
+
+
+class HTTPConflict(HTTPClientError):
+    status_code = 409
+
+
+class HTTPGone(HTTPClientError):
+    status_code = 410
+
+
+class HTTPLengthRequired(HTTPClientError):
+    status_code = 411
+
+
+class HTTPPreconditionFailed(HTTPClientError):
+    status_code = 412
+
+
+class HTTPPayloadTooLarge(HTTPClientError):
+    status_code = 413
+
+
+class HTTPURITooLong(HTTPClientError):
+    status_code = 414
+
+
+class HTTPUnsupportedMediaType(HTTPClientError):
+    status_code = 415
+
+
+class HTTPRangeNotSatisfiable(HTTPClientError):
+    status_code = 416
+
+
+class HTTPExpectationFailed(HTTPClientError):
+    status_code = 417
+
+
+class HTTPMisdirectedRequest(HTTPClientError):
+    status_code = 421
+
+
+class HTTPUnprocessableEntity(HTTPClientError):
+    status_code = 422
+
+
+class HTTPLocked(HTTPClientError):
+    status_code = 423
+
+
+class HTTPFailedDependency(HTTPClientError):
+    status_code = 424
+
+
+class HTTPUpgradeRequired(HTTPClientError):
+    status_code = 426
+
+
+class HTTPPreconditionRequired(HTTPClientError):
+    status_code = 428
+
+
+class HTTPTooManyRequests(HTTPClientError):
+    status_code = 429
+
+
+class HTTPRequestHeaderFieldsTooLarge(HTTPClientError):
+    status_code = 431
+
+
+class HTTPUnavailableForLegalReasons(HTTPClientError):
+    status_code = 451
+
+
+class HTTPInternalServerError(HTTPServerError):
+    status_code = 500
+
+
+class HTTPNotImplemented(HTTPServerError):
+    status_code = 501
+
+
+class HTTPBadGateway(HTTPServerError):
+    status_code = 502
+
+
+class HTTPServiceUnavailable(HTTPServerError):
+    status_code = 503
+
+
+class HTTPGatewayTimeout(HTTPServerError):
+    status_code = 504
+
+
+class HTTPHTTPVersionNotSupported(HTTPServerError):
+    status_code = 505
+
+
+class HTTPVariantAlsoNegotiates(HTTPServerError):
+    status_code = 506
+
+
+class HTTPInsufficientStorage(HTTPServerError):
+    status_code = 507
+
+
+class HTTPLoopDetected(HTTPServerError):
+    status_code = 508
+
+
+class HTTPNotExtended(HTTPServerError):
+    status_code = 510
+
+
+class HTTPNetworkAuthenticationRequired(HTTPServerError):
+    status_code = 511
