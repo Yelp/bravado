459f39d943da549089056c3f892c43f920d0c858
Stephan Jaensch <sjaensch@yelp.com>
2017-06-21 16:20:22 +0200

    Make sure we throw the correct exception when encountering a crochet timeout

--- bravado/fido_client.py
+++ bravado/fido_client.py
@@ -3,6 +3,7 @@ import logging
 
 import crochet
 import fido
+import fido.exceptions
 import requests
 from bravado_core.response import IncomingResponse
 from yelp_bytes import to_bytes
@@ -148,4 +149,8 @@ class FidoFutureAdapter(FutureAdapter):
             return self._eventual_result.wait(timeout=timeout)
         except crochet.TimeoutError:
             self._eventual_result.cancel()
-            raise
+            raise fido.exceptions.HTTPTimeoutError(
+                'Connection was closed by fido after blocking for '
+                'timeout={timeout} seconds waiting for the server to '
+                'send the response'.format(timeout=timeout)
+            )
--- tests/fido_client/FidoFutureAdapter/result_test.py
+++ tests/fido_client/FidoFutureAdapter/result_test.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import crochet
+import fido.exceptions
 import pytest
 from mock import Mock
 
@@ -18,7 +19,8 @@ def test_eventual_result_cancelled_on_exception():
     mock_eventual_result = Mock(wait=Mock(side_effect=crochet.TimeoutError()))
     adapter = FidoFutureAdapter(mock_eventual_result)
 
-    with pytest.raises(crochet.TimeoutError):
-        adapter.result()
+    with pytest.raises(fido.exceptions.HTTPTimeoutError) as exc_info:
+        adapter.result(timeout=1)
 
     assert mock_eventual_result.cancel.called is True
+    assert str(exc_info.value).startswith('Connection was closed by fido after blocking for timeout=1 seconds')
