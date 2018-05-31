dc47775fa3b6a7e089fde050810d9a59f97ea367
Stephan Jaensch <sjaensch@yelp.com>
2018-05-31 16:18:22 +0200

    Add more fallback_result tests

--- tests/http_future/HttpFuture/response_test.py
+++ tests/http_future/HttpFuture/response_test.py
@@ -13,19 +13,28 @@ class ResponseMetadata(BravadoResponseMetadata):
     pass
 
 
-def test_fallback_result(mock_future_adapter):
+@pytest.fixture
+def mock_operation():
+    return mock.Mock(name='operation')
+
+
+@pytest.fixture
+def http_future(mock_future_adapter, mock_operation):
+    response_adapter_instance = mock.Mock(spec=IncomingResponse, status_code=200, swagger_result=None)
+    response_adapter_type = mock.Mock(return_value=response_adapter_instance)
+    return HttpFuture(
+        future=mock_future_adapter,
+        response_adapter=response_adapter_type,
+        operation=mock_operation,
+    )
+
+
+def test_fallback_result(mock_future_adapter, mock_operation, http_future):
     fallback_result = mock.Mock(name='fallback result')
     mock_future_adapter.result.side_effect = BravadoTimeoutError()
-    mock_operation = mock.Mock(name='operation')
     mock_operation.swagger_spec.config = {
         'bravado': BravadoConfig.from_config_dict({'disable_fallback_results': False})
     }
-
-    http_future = HttpFuture(
-        future=mock_future_adapter,
-        response_adapter=mock.Mock(spec=IncomingResponse),
-        operation=mock_operation,
-    )
 
     response = http_future.response(fallback_result=lambda e: fallback_result)
 
@@ -34,36 +43,28 @@ def test_fallback_result(mock_future_adapter):
     assert response.response_metadata.exc_info[0] is BravadoTimeoutError
 
 
-def test_no_fallback_result_if_config_disabled(mock_future_adapter):
+def test_no_fallback_result_if_not_provided(mock_future_adapter, http_future):
     mock_future_adapter.result.side_effect = BravadoTimeoutError()
-    mock_operation = mock.Mock(name='operation')
+
+    with pytest.raises(BravadoTimeoutError):
+        http_future.response()
+
+
+def test_no_fallback_result_if_config_disabled(mock_future_adapter, mock_operation, http_future):
+    mock_future_adapter.result.side_effect = BravadoTimeoutError()
     mock_operation.swagger_spec.config = {
         'bravado': BravadoConfig.from_config_dict({'disable_fallback_results': True})
     }
-
-    http_future = HttpFuture(
-        future=mock_future_adapter,
-        response_adapter=mock.Mock(spec=IncomingResponse),
-        operation=mock_operation,
-    )
 
     with pytest.raises(BravadoTimeoutError):
         http_future.response(fallback_result=lambda e: None)
 
 
-def test_custom_response_metadata(mock_future_adapter):
-    mock_operation = mock.Mock(name='operation')
+def test_custom_response_metadata(mock_operation, http_future):
     mock_operation.swagger_spec.config = {
         'bravado': BravadoConfig.from_config_dict(
             {'response_metadata_class': 'tests.http_future.HttpFuture.response_test.ResponseMetadata'})
     }
-    response_adapter_instance = mock.Mock(spec=IncomingResponse, status_code=200, swagger_result=None)
-    response_adapter_type = mock.Mock(return_value=response_adapter_instance)
-    http_future = HttpFuture(
-        future=mock_future_adapter,
-        response_adapter=response_adapter_type,
-        operation=mock_operation,
-    )
 
     with mock.patch('bravado.http_future.unmarshal_response'):
         response = http_future.response()
