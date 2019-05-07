a0e9fa6896591162b0bd5284ad69df33a8f4934c
Samuele Maci <macisamuele@gmail.com>
2019-05-07 11:56:26 +0200

    Fix tests on windows

--- tests/swagger_model/load_file_test.py
+++ tests/swagger_model/load_file_test.py
@@ -1,4 +1,7 @@
 # -*- coding: utf-8 -*-
+import os
+import sys
+
 import pytest
 
 from bravado.swagger_model import load_file
@@ -30,5 +33,8 @@ def test_spec_internal_representation_identical():
 
 def test_non_existent_file():
     with pytest.raises(IOError) as excinfo:
-        load_file('test-data/2.0/i_dont_exist.json')
-    assert 'No such file or directory' in str(excinfo.value)
+        load_file(os.path.join('test-data', '2.0', 'i_dont_exist.json'))
+    if sys.platform == "win32":
+        assert 'The system cannot find the file specified' in str(excinfo.value)
+    else:
+        assert 'No such file or directory' in str(excinfo.value)
