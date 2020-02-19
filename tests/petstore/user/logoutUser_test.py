6e284f293d738a2002d616890c76449152cd16d9
Samuele Maci <macisamuele@gmail.com>
2020-02-19 14:30:05 +0100

    Remove petstore integration tests as it makes tests dependent on external services

--- tests/petstore/user/logoutUser_test.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-import pytest
-
-
-@pytest.mark.xfail(reason='Uh, the spec does not specify any parameters')
-def test_200_success(petstore):
-    result = petstore.user.logoutUser().result()
-    assert result is None
