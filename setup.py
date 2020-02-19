490532490e5111cb5d08a7612eb1c2c5afb2db22
Samuele Maci <macisamuele@gmail.com>
2020-02-19 11:53:29 +0100

    Run tests on Python 3.7 as well

--- setup.py
+++ setup.py
@@ -29,6 +29,7 @@ setup(
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
     ],
     package_data={
         'bravado': ['py.typed'],
