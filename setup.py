dc9d72fb891b4bc04b69927cc0684b010bfd8923
vesivaku <117249946+vesivaku@users.noreply.github.com>
2023-12-04 13:40:22 +0530

    Add bravado version for Python 3.11 support

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ import bravado
 setup(
     name="bravado",
     # cloudlock version, with no twisted dependency
-    version=bravado.version + "+py311",
+    version="4.11.0",
     license="BSD 3-Clause License",
     description="Library for accessing Swagger-enabled API's",
     long_description=open(os.path.join(os.path.dirname(__file__),
